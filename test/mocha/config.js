043b34b37abb792be3adf92f0f3ead969ed5ce58
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-08-13 12:09:41 -0400

    more tests for the config ware

--- test/mocha/config.js
+++ test/mocha/config.js
@@ -6,8 +6,8 @@ var chai = require('chai'),
 describe('Config:', function () {
     var options, req, res, basePath = '/kpm-config.json';
 
-    var apiWrapper = function(o) {
-        return function() {
+    var apiWrapper = function (o) {
+        return function () {
             publishApi(o);
         };
     };
@@ -48,8 +48,7 @@ describe('Config:', function () {
     describe('Options', function () {
 
         it('should use defaults when not supplied', function (done) {
-            res.send = function(status, data) {
-                expect(status).to.equal(200);
+            res.send = function (status, data) {
                 expect(data.packages).to.exist;
                 expect(data.publishing).to.exist;
                 expect(data.owner).to.exist;
@@ -57,6 +56,29 @@ describe('Config:', function () {
             };
 
             config({})(req, res, null);
+        });
+    });
+
+    describe('Middleware', function () {
+
+        it('should return 200', function (done) {
+            res.send = function (status, data) {
+                expect(status).to.equal(200);
+                done();
+            };
+
+            config(options)(req, res, null);
+        });
+
+        it('should return a json object', function (done) {
+            res.send = function (status, data) {
+                expect(data.packages).to.equal(options.packages);
+                expect(data.publishing).to.equal(options.publishing);
+                expect(data.owner).to.equal(options.owner);
+                done();
+            };
+
+            config(options)(req, res, null);
         });
     });
 });
